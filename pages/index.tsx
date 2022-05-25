ce597e98a392306f4e1ba959487841777caea5c7
stvallen25 <stvallen25@gmail.com>
2022-05-24 23:47:18 -0500

    add basic ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,46 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
-import { Box } from '@mui/material';
+import { Box, Container, TextField, Button } from '@mui/material';
 import { Header } from '../layouts/header';
 
 const Home: NextPage = () => {
   return (
     <Box>
       <Header />
+      <Container
+        maxWidth="lg"
+        sx={{ paddingTop: '100px', display: 'flex', justifyContent: 'center' }}
+      >
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            width: '250px',
+          }}
+        >
+          <Box sx={{ width: '100%' }}>
+            <TextField
+              id="outlined-basic"
+              label="DAI Amount"
+              variant="outlined"
+              fullWidth
+            />
+          </Box>
+          <Box sx={{ width: '100%', mt: 1 }}>
+            <TextField
+              id="outlined-basic"
+              label="Receipient Address"
+              variant="outlined"
+              fullWidth
+            />
+          </Box>
+          <Button variant="contained" fullWidth sx={{ mt: 1 }}>
+            Send
+          </Button>
+        </Box>
+      </Container>
     </Box>
   );
 };
